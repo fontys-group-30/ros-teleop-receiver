3501f4083b3d8bff42292697b1a12ff04c8e2918
PauloRC123 <103492828+PauloRC123@users.noreply.github.com>
2024-10-01 12:40:20 +0200

    Update receiver_node.py
    fixed variable names of the wheels and wheel order

--- teleop_receiver/receiver_node.py
+++ teleop_receiver/receiver_node.py
@@ -30,11 +30,11 @@ class ReceiverNode(Node):
                                f"Angular: x={msg.angular.x}, y={msg.angular.y}, z={msg.angular.z}")
 
         # Calculate and log wheel velocities
-        wheel1, wheel2, wheel3, wheel4 = self.compute_wheel_velocities(msg)
+        wheel_front_left, wheel_front_right, wheel_back_left, wheel_back_right = self.compute_wheel_velocities(msg)
 
         # Send the wheel velocities to the Arduino
         try:
-            message_str = f"IN: {round(wheel1, 1)},{round(wheel2, 1)},{round(wheel3, 1)},{round(wheel4, 1)} \n"
+            message_str = f"IN: {round(wheel_front_left, 1)},{round(wheel_front_right, 1)},{round(wheel_back_left, 1)},{round(wheel_back_right, 1)} \n"
             self.serial_connection.write(message_str.encode())
             self.get_logger().info(message_str)
         except Exception as e:
@@ -42,11 +42,11 @@ class ReceiverNode(Node):
 
     def compute_wheel_velocities(self, msg: Twist):
         # Calculate the angular velocity of the wheels in rad/s
-        wheel1 = (msg.linear.x - msg.linear.y - msg.angular.z) / 0.08
-        wheel2 = (msg.linear.x + msg.linear.y + msg.angular.z) / 0.08
-        wheel3 = (msg.linear.x - msg.linear.y + msg.angular.z) / 0.08
-        wheel4 = (msg.linear.x + msg.linear.y - msg.angular.z) / 0.08
-        return wheel1, wheel2, wheel3, wheel4
+        wheel_front_left = (msg.linear.x - msg.linear.y - msg.angular.z) / 0.08
+        wheel_front_right = (msg.linear.x + msg.linear.y + msg.angular.z) / 0.08
+        wheel_back_left = (msg.linear.x + msg.linear.y - msg.angular.z) / 0.08
+        wheel_back_right = (msg.linear.x - msg.linear.y + msg.angular.z) / 0.08
+        return wheel_front_left, wheel_front_right, wheel_back_left, wheel_back_right
 
 def main(args=None):
     rclpy.init(args=args)
