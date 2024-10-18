16dc421f2a1944a307139f5555e83e3b13bfa248
Reno Muijsenberg <490412@student.fontys.nl>
2024-10-18 13:25:15 +0200

    sub to /cmd vel nac

--- teleop_receiver/receiver_node.py
+++ teleop_receiver/receiver_node.py
@@ -21,7 +21,7 @@ class ReceiverNode(Node):
             return
         self.subscription = self.create_subscription(
             Twist,
-            '/cmd_vel',
+            '/cmd_vel_nav',
             self.receiver_callback,
             10)
 
